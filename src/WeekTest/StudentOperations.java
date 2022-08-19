25133b281cbe5d6e327a81d4894db5bf7e6ce731
devayush-bajaj-coditas <devayush.bajaj@coditas.com>
2022-08-19 17:13:51 +0100

    InterviewTEst

--- src/WeekTest/StudentOperations.java
+++ src/WeekTest/StudentOperations.java
@@ -5,12 +5,9 @@ import java.util.Scanner;
 public class StudentOperations {
 StudentOperations studentOperations = new StudentOperations();
     int noOfStudents;
+    Student student [] = new Student[5];
     Scanner s =new Scanner(System.in);
     void insert(){
-        System.out.println("Enter number of Students you want to insert");
-        noOfStudents = s.nextInt();
-
-        Student student [] = new Student[noOfStudents];
         for(int i = 0; i< student.length-1 ;i ++){
             student[i] = new Student(student[i].id,student[i].firstName,student[i].lastName,student[i].city,student[i].trackStack,student[i].mobileNo);
         }
@@ -24,6 +21,11 @@ StudentOperations studentOperations = new StudentOperations();
     void remove(){
         System.out.println("Enter id which you want to remove");
         int removeId= s.nextInt();
+        for(int i =0 ; i< student.length -1 ; i++){
+            if(student[0].equals(removeId)){
+
+            }
+        }
     }
     void fetch(){
 
